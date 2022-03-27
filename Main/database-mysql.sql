e075813b8c3f42975ced932a4c85c534fdf0138a
Nasyrov Takhir <64357944+nasyrovt@users.noreply.github.com>
2022-03-28 01:49:52 +0200

    Update database-mysql.sql

--- Main/database-mysql.sql
+++ Main/database-mysql.sql
@@ -1,67 +1,80 @@
-CREATE TABLE tbl_abonne (
-                            abonne_id int NOT NULL,
-                            nom varchar(45) NOT NULL,
-                            prenom varchar(35) NOT NULL,
-                            dateDeN datetime NOT NULL,
-                            sexe int NOT NULL,
-                            adresse varchar(50) NOT NULL,
-                            final_reduction float NOT NULL,
-                            date_debut_abonnement datetime NOT NULL,
-                            date_end_abonnement datetime NOT NULL,
-                            prime float DEFAULT NULL,
-                            abonneId bigint NOT NULL,
-                            date_den datetime(6) DEFAULT NULL,
-                            PRIMARY KEY (abonne_id)
+CREATE TABLE `client_sequence` (
+  `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE tbl_station (
-                             station_id int NOT NULL,
-                             adresse varchar(50) NOT NULL,
-                             calssidication varchar(35) NOT NULL,
-                             classification int DEFAULT NULL,
-                             PRIMARY KEY (station_id)
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE tbl_bornette (
-                              bornette_id int NOT NULL,
-                              etat_bornette varchar(35) NOT NULL,
-                              station_mere int NOT NULL,
-                              bornetteId bigint NOT NULL,
-                              PRIMARY KEY (bornette_id),
-    KEY fk_station_mere_idx (station_mere),
-    CONSTRAINT fk_station_mere FOREIGN KEY (station_mere) REFERENCES tbl_station (station_id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE tbl_client (
-                            client_id int NOT NULL,
-                            numero_cb int NOT NULL,
-                            code_secret varchar(45) DEFAULT NULL,
-                            PRIMARY KEY (client_id)
+CREATE TABLE `tbl_station` (
+  `station_id` bigint NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `classification` int DEFAULT NULL,
+  PRIMARY KEY (`station_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE tbl_location (
-                              location_id int NOT NULL,
-                              start_date datetime NOT NULL,
-                              end_date datetime NOT NULL,
-                              station_depart int NOT NULL,
-                              station_arrive int DEFAULT NULL,
-                              PRIMARY KEY (location_id),
-    KEY fk_station_depart_idx (station_depart),
-    KEY fk_station_arrive_idx (station_arrive),
-    CONSTRAINT fk_station_arrive FOREIGN KEY (station_arrive) REFERENCES tbl_station (station_id),
-    CONSTRAINT fk_station_depart FOREIGN KEY (station_depart) REFERENCES tbl_station (station_id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tbl_client` (
+  `client_id` bigint NOT NULL AUTO_INCREMENT,
+  `code_secret` varchar(255) DEFAULT NULL,
+  `numero_cb` bigint DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=303 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `tbl_velo` (
+  `velo_id` bigint NOT NULL AUTO_INCREMENT,
+  `etat_de_velo` int DEFAULT NULL,
+  `date_de_mise_en_service` datetime DEFAULT NULL,
+  `nom_du_modele` varchar(255) DEFAULT NULL,
+  `prix_du_modele` float DEFAULT NULL,
+  `numero_de_puce` int NOT NULL,
+  PRIMARY KEY (`velo_id`),
+  UNIQUE KEY `puce_unique` (`numero_de_puce`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-CREATE TABLE tbl_velo (
-                          velo_id int NOT NULL,
-                          nom_du_modele varchar(45) NOT NULL,
-                          prix_du_modele float NOT NULL,
-                          etat_de_velo varchar(45) NOT NULL,
-                          date_de_mise_en_service datetime NOT NULL,
-                          numero_de_puce int NOT NULL,
-                          veloId bigint NOT NULL,
-                          PRIMARY KEY (velo_id),
-                          UNIQUE KEY puce_unique (numero_de_puce)
+CREATE TABLE `tbl_abonne` (
+  `abonne_id` bigint NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `date_den` datetime DEFAULT NULL,
+  `date_debut_abonnement` datetime DEFAULT NULL,
+  `date_end_abonnement` datetime DEFAULT NULL,
+  `final_reduction` float DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  `prime` float DEFAULT NULL,
+  `sexe` int DEFAULT NULL,
+  PRIMARY KEY (`abonne_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `tbl_bornette` (
+  `bornette_id` bigint NOT NULL AUTO_INCREMENT,
+  `etat_bornette` int DEFAULT NULL,
+  `station_mere` bigint DEFAULT NULL,
+  `current_velo` bigint DEFAULT NULL,
+  `tbl_bornettecol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`bornette_id`),
+  KEY `FK5emwhr8pf64n7iprdoivi9u3x` (`station_mere`),
+  KEY `FK_Velo_idx` (`current_velo`),
+  CONSTRAINT `FK5emwhr8pf64n7iprdoivi9u3x` FOREIGN KEY (`station_mere`) REFERENCES `tbl_station` (`station_id`),
+  CONSTRAINT `FK_Velo` FOREIGN KEY (`current_velo`) REFERENCES `tbl_velo` (`velo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `tbl_location` (
+  `location_id` bigint NOT NULL AUTO_INCREMENT,
+  `end_date` datetime DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `station_arrive` bigint DEFAULT NULL,
+  `station_depart` bigint DEFAULT NULL,
+  `client_id` bigint DEFAULT NULL,
+  `velo_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`location_id`),
+  KEY `FKs5qxj7u1qaxe5ufcc4chyn82q` (`station_arrive`),
+  KEY `FK_id_client_idx` (`client_id`),
+  KEY `FK_id_velo_idx` (`velo_id`),
+  KEY `FKbgoi8k6rh01dsy5p3wgjuobvo` (`station_depart`),
+  CONSTRAINT `FKbgoi8k6rh01dsy5p3wgjuobvo` FOREIGN KEY (`station_depart`) REFERENCES `tbl_station` (`station_id`),
+  CONSTRAINT `FKip5a7ufcu0rl1ximw4wcj0urr` FOREIGN KEY (`client_id`) REFERENCES `tbl_client` (`client_id`),
+  CONSTRAINT `FKlmb5ix40l38d1blbm4csea9eb` FOREIGN KEY (`velo_id`) REFERENCES `tbl_velo` (`velo_id`),
+  CONSTRAINT `FKs5qxj7u1qaxe5ufcc4chyn82q` FOREIGN KEY (`station_arrive`) REFERENCES `tbl_station` (`station_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
